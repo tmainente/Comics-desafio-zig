490609dca11e6ec561acdc9ce09112fe3e1b3d31
“tmainenteguerra” <thiagomainente@hotmail.com>
2025-04-24 11:32:26 -0300

    removendo libs nao utilizadas

--- app/src/main/java/com/example/comics/data/remote/util/NetworkBoundResource.kt
+++ app/src/main/java/com/example/comics/data/remote/util/NetworkBoundResource.kt
@@ -16,7 +16,6 @@ inline fun <ResultType, RequestType> networkBoundResource(
 
     val flow = if (shouldFetch(data)) {
         emit(Resource.Loading(data))
-
         try {
             saveFetchResult(fetch())
             query().map { Resource.Success(it) }
